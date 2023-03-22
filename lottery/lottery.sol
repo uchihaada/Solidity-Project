0e6addbda96c0b4fa4accb77623a4dadc4a8b2fa
Abhijit <abhijitadadas@gmail.com>
2023-03-22 19:52:04 +0530

    final commit

--- lottery/lottery.sol
+++ lottery/lottery.sol
@@ -11,9 +11,24 @@ contract Lottery{
     }
 
     receive() external payable{
+        require(msg.value==0.0000000001 ether);
         participants.push(payable(msg.sender));
     }
     function getBalance() public view returns(uint){
+        require(msg.sender==manager);
         return address(this).balance;
+    }
+    function random() public view returns(uint){
+       return  uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
+    }
+    function selectWinner() public {
+        require(msg.sender==manager);
+        require(participants.length>=3);
+        uint r=random();
+        uint index = r % participants.length;
+        address payable winner;
+        winner=participants[index];
+        winner.transfer(getBalance());
+        participants=new address payable[] (0);
     }
 }
